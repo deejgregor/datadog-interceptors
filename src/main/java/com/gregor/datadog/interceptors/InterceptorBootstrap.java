bc88750f93bd24a31a731945f5ced1bd6e7b133c
DJ Gregor <dj@gregor.com>
2024-10-06 19:02:46 -0700

    Rename TagMatchingErrorInterceptor -> SetErrorInterceptor
    This just fits better with UnsetErrorInterceptor

--- src/main/java/com/gregor/datadog/interceptors/InterceptorBootstrap.java
+++ src/main/java/com/gregor/datadog/interceptors/InterceptorBootstrap.java
@@ -7,7 +7,7 @@ public final class InterceptorBootstrap {
     @SuppressWarnings("unused")
     public static void premain(String agentArgs) {
         GlobalTracer.get().addTraceInterceptor(new UnsetErrorInterceptor(100));
-        GlobalTracer.get().addTraceInterceptor(new TagMatchingErrorInterceptor(101));
+        GlobalTracer.get().addTraceInterceptor(new SetErrorInterceptor(101));
 
         // Promote comes last, so it can act on any adjustments to the error status made earlier
         GlobalTracer.get().addTraceInterceptor(new PromoteErrorInterceptor(102));
--- src/main/java/com/gregor/datadog/interceptors/SetErrorInterceptor.java
+++ src/main/java/com/gregor/datadog/interceptors/SetErrorInterceptor.java
@@ -7,14 +7,14 @@ import java.util.Map.Entry;
 
 import datadog.trace.api.interceptor.MutableSpan;
 
-class TagMatchingErrorInterceptor extends AbstractTraceInterceptor {
+class SetErrorInterceptor extends AbstractTraceInterceptor {
     // The key is the *new* tag we are going to create by copying the value from the *existing* tag in the value
     private Map<String, String> tagMapping = new HashMap<String, String>();
 
-    public TagMatchingErrorInterceptor(int priority) {
+    public SetErrorInterceptor(int priority) {
         super(priority);
 
-        String mappingList = getConfig("dd.error.tagmatching.mapping");
+        String mappingList = getConfig("dd.error.set.mapping");
         if (mappingList != null) {
             for (String mapping : mappingList.split(",")) {
                 String[] split = mappingList.split("=");
@@ -27,7 +27,7 @@ class TagMatchingErrorInterceptor extends AbstractTraceInterceptor {
     }
     @Override
     public String getPropertyPrefix() {
-        return "dd.error.tagmatching.pattern.";
+        return "dd.error.set.pattern.";
     }
 
     @Override
@@ -37,7 +37,7 @@ class TagMatchingErrorInterceptor extends AbstractTraceInterceptor {
             // We don't care if it's an error already, always run in case there's tag mapping to do
             if (spanMatchesPatterns(span)) {
                 span.setError(true);
-                span.setTag("error.tagmatching", "true");
+                span.setTag("error.set", "true");
                 for (Entry<String, String> mapping : tagMapping.entrySet()) {
                     Object tagValue = span.getTag(mapping.getValue());
                     if (tagValue != null) {
